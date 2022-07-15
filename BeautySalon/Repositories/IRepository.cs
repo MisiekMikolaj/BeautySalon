8c13c30a906a64e95647d30ab8741d91c6b878b2
MisiekMikolaj <102438726+MisiekMikolaj@users.noreply.github.com>
2022-07-15 10:20:33 +0200

    Delete BeautySalon/Repositories directory

--- BeautySalon/Repositories/IRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using BeautySalon.Entities;
-
-namespace BeautySalon.Repositories
-{
-    public interface IRepository<T> : IReadRepository<T>, IWriteRepository<T> 
-        where T : class, IEntity
-    {   
-    }
-}
